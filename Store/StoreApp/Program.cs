7394ed18e3c028da23b6ba1f7c47a69fd43723fb
berkay <berkayacc74@gmail.com>
2023-08-28 12:22:12 +0300

    githuba proje gönderiyorum

---
+++ Store/StoreApp/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddDbContext<RepositoryContext>(options =>
+        {
+            options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+        });
+        
+        var app = builder.Build();
+
+        app.UseStaticFiles();
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+        app.Run();
+    }
+}
